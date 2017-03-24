c0d42085a23a1d65d2ad2b1304a051497977ccbb
adam-cheqitout <adam.m.denny2@gmail.com>
2017-03-24 12:48:52 -0700

    adding in animations and a sound

---
+++ Assets/playaudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playaudio : MonoBehaviour {
+    private AudioSource bang;
+	// Use this for initialization
+	void Start () {
+        bang = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            bang.Play();
+        }
+	}
+}
